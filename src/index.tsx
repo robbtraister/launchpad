043b0fbcd627f61c39f701c6a38e34d8695a0444
Robb Traister <rtraister@citrine.io>
2021-10-21 09:47:48 -0400

    add redirect support

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import { Pages } from "./pages";
 
 import "./index.scss";
 
+// if sent here from the 404 page, update the history URI
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  history.replaceState(null, "", redirect);
+}
+
 window.document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
     <StrictMode>
