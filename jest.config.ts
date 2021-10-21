cdfe91b19532dcfb937e26e4542c8a4fecc6bb89
Robb Traister <rtraister@citrine.io>
2021-10-21 09:52:03 -0400

    better jest coverage support

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 export default {
   roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  moduleFileExtensions: ["tsx", "ts", "jsx", "js", "json"],
+  moduleNameMapper: {
+    "\\.(scss)$": "identity-obj-proxy",
   },
   testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "\\.tsx?$": "ts-jest",
+  },
 };
